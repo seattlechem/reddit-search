6811ff647b290af09f61cb621f61e3fe3e59fc24
Peter Kim <seattlechem@yahoo.com>
2018-05-17 05:36:58 -0700

    searchform, searchresult, superagent added/installed

--- reddit-search/src/App.js
+++ reddit-search/src/App.js
@@ -1,21 +1,26 @@
-import React, { Component } from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { Component } from 'react'
+import SearchForm from './SearchForm/search-form'
+import SearchResult from './SearchResultList/search-result'
+import './App.css'
 
-class App extends Component {
-  render() {
-    return (
-      <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <h1 className="App-title">Welcome to React</h1>
-        </header>
-        <p className="App-intro">
-          To get started, edit <code>src/App.js</code> and save to reload.
-        </p>
-      </div>
-    );
+export default class App extends Component {
+    constructor(props) {
+      super(props)
+      this.state = {
+        results: undefined,
+      }
+    }
+  
+    render() {
+      return (
+        <div className="App">
+          <h1>Search Reddit</h1>
+          <SearchForm setAppState={this.setState.bind(this)} />
+          { this.state.results ?
+          <SearchResult results={this.state.results} />
+          : undefined
+          }
+        </div>
+      )
+    }
   }
-}
-
-export default App;
